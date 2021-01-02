691e6d433b73592a438f6b540e64f10e34a7ad29
Robin Opletal <me@robinopletal.com>
2021-01-02 01:00:56 +0100

    Move sleep to the checker script

--- ci-check.sh
+++ ci-check.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -e
 
+## SPECIFY CONTAINER NAME AS FIRST ARGUMENT, SLEEP TIME AS SECOND
+
+sleep $2
+
 container_name="$1"
 
 if [ $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ];
 then
+  echo "The container is running - all OK"
   exit 0
 else
+  echo "The container is not running; it probably failed when starting"
+  echo "Here are the logs:"
   echo $(docker logs $container_name)
   exit 1
 fi
