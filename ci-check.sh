4de98454b27d6f3270bc38f18309d72a84447c43
Robin Opletal <me@robinopletal.com>
2021-01-04 13:09:48 +0100

    Add logs to CI on success as well

--- ci-check.sh
+++ ci-check.sh
@@ -10,6 +10,8 @@ container_name="$1"
 if [ $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ];
 then
   echo "The container is running - all OK"
+  echo "Here are the logs:"
+  echo $(docker logs $container_name)
   exit 0
 else
   echo "The container is not running; it probably failed when starting"
